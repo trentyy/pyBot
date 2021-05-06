e625c9d4b28445b5856bbce09437a9764fc2f85e
trentyy <trent.y.yang@gmail.com>
2021-05-06 23:58:18 +0800

    fix bugs

--- cmds/yt_waiting_room.py
+++ cmds/yt_waiting_room.py
@@ -70,8 +70,8 @@ class  ytWaitingRoom(Cog_Extension):
             message = await channel.fetch_message(data.message_id)
                         
             await message.remove_reaction(data.emoji, data.member)
-            waiting_sql = "SELECT videoId, scheduledStartTime FROM `videos` WHERE `scheduledStartTime` IS NOT NULL AND (`actualStartTime` IS NULL OR `actualEndTime` IS NULL);"
-            live_sql = "SELECT videoId, scheduledStartTime FROM `videos` WHERE `scheduledStartTime` IS NOT NULL AND (`actualStartTime` IS NOT NULL AND `actualEndTime` IS NULL);"
+            waiting_sql = "SELECT videoId, scheduledStartTime FROM `videos` WHERE `scheduledStartTime` IS NOT NULL AND `actualStartTime` IS NULL AND `actualEndTime` IS NULL;"
+            live_sql = "SELECT videoId, scheduledStartTime FROM `videos` WHERE `scheduledStartTime` IS NOT NULL AND `actualStartTime` IS NOT NULL AND `actualEndTime` IS NULL;"
             upcoming_videos = self.tracker.execute(waiting_sql)
             live_videos = self.tracker.execute(live_sql)
             print("upcoming videos: ", upcoming_videos)
@@ -93,8 +93,8 @@ class  ytWaitingRoom(Cog_Extension):
             self.msg_upcoming = await channel.fetch_message(826197268055064576)
             self.msg_live = await channel.fetch_message(826197370353614911)
             while not self.bot.is_closed():
-                waiting_sql = "SELECT `videoId`, `scheduledStartTime` WHERE `scheduledStartTime` IS NOT NULL AND (`actualStartTime` IS NULL OR `actualEndTime` IS NULL);"
-                live_sql = "SELECT `videoId`, `scheduledStartTime` WHERE `scheduledStartTime` IS NOT NULL AND (`actualStartTime` IS NOT NULL AND `actualEndTime` IS NULL);"
+                waiting_sql = "SELECT `videoId`, `scheduledStartTime` WHERE `scheduledStartTime` IS NOT NULL AND `actualStartTime` IS NULL AND `actualEndTime` IS NULL;"
+                live_sql = "SELECT `videoId`, `scheduledStartTime` WHERE `scheduledStartTime` IS NOT NULL AND  `actualStartTime` IS NOT NULL AND `actualEndTime` IS NULL;"
                 upcoming_videos = self.tracker.execute(waiting_sql)
                 live_videos = self.tracker.execute(live_sql)
                 
